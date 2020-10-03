6bfee6e562a6d4b29a81b1f85ae9286bc86e64c0
冯毅然 <fangyira@gmail.com>
2020-10-03 18:42:03 +0800

    Update main.go

--- main.go
+++ main.go
@@ -7,6 +7,9 @@ import (
 func main() {
 	v := mediator.M.VideoTranscode()
 	v.SetCrf(32)
-	v.SetInputFile("/Volumes/MyBook_Data/Phone_Video1.mov")
-	v.Run()
+	tasklist := []string{"/Volumes/MyBook_Data/Phone_Video1.mov"}
+	for _, task := range tasklist {
+		v.SetInputFile(task)
+		v.Run()
+	}
 }
